080d7404c0dd0b62f166d273fdf904a15bb9b464
Kate Kondratiuk <katia.kondratiuk1111@gmail.com>
2020-06-04 16:23:41 +0300

    Create JavaFX project using jre 11.

---
+++ SquiGoIdea/src/Frontend/Main.java
@@ -0,0 +1,25 @@
+package Frontend;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.SequenceInputStream;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        stage.setTitle("SquiGo");
+        stage.setScene(new Scene(root, 300, 275));
+        stage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
