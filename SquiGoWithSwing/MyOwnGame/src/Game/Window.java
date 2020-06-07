3e1f711f11d4ccfc3f7c75a2c46c16a7f2e32c11
Kate Kondratiuk <katia.kondratiuk1111@gmail.com>
2020-06-07 08:17:22 +0300

    Renamed

---
+++ SquiGoWithSwing/MyOwnGame/src/Game/Window.java
@@ -0,0 +1,24 @@
+package com.main;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Window extends Canvas {
+    public Window (int WIGTH, int HEIGTH, String title, Game game){
+        JFrame frame = new JFrame(title);
+        frame.setPreferredSize(new Dimension(WIGTH, HEIGTH));
+        frame.setMaximumSize(new Dimension(WIGTH, HEIGTH));
+        frame.setMinimumSize(new Dimension(WIGTH, HEIGTH));
+
+
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.add(game);
+        frame.setVisible(true);
+        game.start();
+
+
+    }
+
+}
