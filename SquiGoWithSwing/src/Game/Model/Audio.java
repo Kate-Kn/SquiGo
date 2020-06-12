02996e92f1cdbc0f259a688a5083fb270910735b
Sasha <zolotarevych.o@gmail.com>
2020-06-12 12:07:06 +0300

    Audio class

--- SquiGoWithSwing/src/Game/Model/Audio.java
+++ SquiGoWithSwing/src/Game/Model/Audio.java
@@ -1,1 +1,33 @@
+package Model;
 
+import org.newdawn.slick.Music;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.Sound;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Audio {
+
+    public static Map<String, Sound> soundMap = new HashMap<String, Sound>();
+    public static Map<String, Music> musicMap = new HashMap<String, Music>();
+
+    public static void loadmusic() {
+        try {
+            soundMap.put("menu_sound", new Sound("resources/music/trialclick.ogg"));
+            musicMap.put("music_menu", new Music("resources/music/menu.ogg"));
+            musicMap.put("music_game", new Music ("resources/music/game.ogg"));
+            soundMap.put("nut", new Sound("resources/music/nutsound.ogg"));
+
+        } catch (SlickException e) {
+            e.printStackTrace();
+        }
+    }
+public static Music getMusic(String key){
+        return musicMap.get(key);
+}
+public static Sound getSound(String key){
+        return soundMap.get(key);
+}
+
+}
