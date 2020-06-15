25a94935e45b76c35429ca282d344d6e9814ea8c
peachypatch <66318280+peachypatch@users.noreply.github.com>
2020-06-15 15:06:40 +0300

    error fixed

--- SquiGoWithSwing/src/Game/Model/Menu.java
+++ SquiGoWithSwing/src/Game/Model/Menu.java
@@ -350,7 +350,7 @@ public class Menu extends MouseAdapter {
                          e.printStackTrace();
            }
            g.drawImage(imagem, -10, 0, 660, 480, null);
-           
+
             Font fnt = new Font("arial", 1, 50);
             Font fnt2 = new Font("arial", 1, 30);
             g.setFont(fnt);
@@ -381,7 +381,7 @@ public class Menu extends MouseAdapter {
           BufferedImage imagem = new BufferedImage(10, 10, 12);
             try {
 
-                imagem = ImageIO.read(getClass().getResource("/resources/backs/menutrialex2.png"));
+                imagem = ImageIO.read(getClass().getResource("/resources/backs/menuselect.png"));
             } catch (Exception e) {
                 e.printStackTrace();
             }
