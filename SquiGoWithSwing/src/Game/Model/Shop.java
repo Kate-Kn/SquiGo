c8916a25019228e064cb8426674d506ff7083936
peachypatch <66318280+peachypatch@users.noreply.github.com>
2020-06-16 03:07:51 +0300

    shop final edit

--- SquiGoWithSwing/src/Game/Model/Shop.java
+++ SquiGoWithSwing/src/Game/Model/Shop.java
@@ -20,11 +20,12 @@ public class Shop extends MouseAdapter {
 
     public void render(Graphics g) {
         g.setColor(Color.white);
-        g.setFont(new Font("arial", 0, 48));
-        g.drawString("SHOP", Game.WIDTH / 2 - 100, 50);
+        g.setFont(new Font("arial", 1, 48));
+        g.drawString("SHOP", 260, 50);
 
-        g.setFont(new Font("arial", 0, 12));
-        g.drawRect(50, 100, 150, 150);
+        g.setFont(new Font("arial", 0, 15));
+        g.drawRect(50, 100, 170, 150);
+        g.setColor(new Color(97,165, 177));
         g.drawString("Upgrade Health (+10%) ", 60, 120);
         g.drawString("Cost : 1 nut", 60, 140);
         BufferedImage imag = new BufferedImage(9, 9, 12);
@@ -35,8 +36,9 @@ public class Shop extends MouseAdapter {
             e.printStackTrace();
         }
         g.drawImage(imag, 75, 175, null);
-
+        g.setColor(Color.white);
         g.drawRect(250, 100, 150, 150);
+        g.setColor(new Color(97,165, 177));
         g.drawString("Upgrade Speed", 260, 120);
         g.drawString("Cost : 1 nut", 260, 140);
         BufferedImage ima = new BufferedImage(9, 9, 12);
@@ -48,8 +50,9 @@ public class Shop extends MouseAdapter {
         }
         g.drawImage(ima, 275, 175, null);
 
-
+        g.setColor(Color.white);
         g.drawRect(450, 100, 150, 150);
+        g.setColor(new Color(97,165, 177));
         g.drawString("Refill Health", 460, 120);
         g.drawString("Cost : " + p3 + " nuts", 460, 140);
 
@@ -62,24 +65,25 @@ public class Shop extends MouseAdapter {
         }
         g.drawImage(image, 475, 175, null);
 
-
+        g.setColor(Color.white);
         g.drawRect(250, 270, 150, 150);
+        g.setColor(new Color(97,165, 177));
         g.drawString("Skip level " + hud.getLevel(), 260, 290);
         g.drawString("Cost : " + p4 + " nuts", 260, 310);
         BufferedImage im = new BufferedImage(9, 9, 12);
         try {
             im = ImageIO.read(getClass().getResource("/resources/shop/enemy_buy.png"));
-
-        } catch (Exception e) {
+          } catch (Exception e) {
             e.printStackTrace();
         }
         g.drawImage(im, 270, 315, null);
 
-
-        g.drawString("Nuts: " + hud.getNuts(), Game.WIDTH - 150, 400);
-        g.drawString("Health: " + hud.HEALTH, Game.WIDTH - 150, 415);
-        g.drawString("Speed: " + handler.speed, Game.WIDTH - 150, 385);
-        g.drawString("Press s to go back", Game.WIDTH - 150, 430);
+        g.setColor(new Color(97,165, 177));
+        g.setFont(new Font("arial", 0, 17));
+        g.drawString("Nuts: " + hud.getNuts(), 475, 400);
+        g.drawString("Health: " + hud.HEALTH, 475, 420);
+        g.drawString("Speed: " + handler.speed, 475, 380);
+        g.drawString("Press s to go back", 475, 440);
     }
 
     public void mousePressed(MouseEvent e) {
@@ -89,6 +93,7 @@ public class Shop extends MouseAdapter {
             if (mx >= 50 && mx < 200) {
                 if (my >= 100 && my <= 250) {
                     if (hud.HEALTH < 100 && hud.getNuts() >= 1) {
+                      Audio.getSound("shop_s").play();
                         hud.setNuts(hud.getNuts() - 1);
                         if (hud.HEALTH <= 90)
                             hud.HEALTH = hud.HEALTH + 10;
@@ -100,6 +105,7 @@ public class Shop extends MouseAdapter {
             if (mx > 250 && mx < 400) {
                 if (my >= 100 && my <= 250) {
                     if (hud.getNuts() >= 1) {
+                      Audio.getSound("shop_s").play();
                         hud.setNuts(hud.getNuts() - 1);
                         handler.speed++;
                     }
@@ -108,6 +114,7 @@ public class Shop extends MouseAdapter {
             if (mx >= 450 && mx <= 600) {
                 if (my >= 100 && my <= 250) {
                     if (hud.getNuts() >= p3 && hud.HEALTH < 100) {
+                      Audio.getSound("shop_s").play();
                         hud.setNuts(hud.getNuts() - p3);
                         p3++;
                         hud.HEALTH = 100;
@@ -118,6 +125,7 @@ public class Shop extends MouseAdapter {
                 if (my >= 270 && my <= 420) {
                     if (hud.getNuts() >= p4 && !isSk) {
                         isSk = true;
+                        Audio.getSound("shop_s").play();
                         hud.setNuts(hud.getNuts() - p4);
                         p4++;
                         if (hud.getLevel() <= 4) {
@@ -139,4 +147,3 @@ public class Shop extends MouseAdapter {
     }
 
 }
-
